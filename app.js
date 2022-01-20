4e2a40caa0ce463e1a8ea8974c149a587a2bc991
Hendell Costa <86265741+hendellcosta@users.noreply.github.com>
2022-01-20 12:10:47 +0100

    moved all items

---
+++ app.js
@@ -0,0 +1,20 @@
+var productBox = document.getElementById("productBox");
+
+function moveToRight(){
+    productBox.style.transform = "rotateY(180deg)";
+    firstImg.style.left = "650px";
+    secondImg.style.left = "20px";
+    firstImg.style.transform = "rotate(-30deg)"
+    secondImg.style.transform = "rotate(0deg)"
+}
+
+function moveToLeft(){
+    productBox.style.transform = "rotateY(0deg)";
+    firstImg.style.left = "20px";
+    secondImg.style.left = "-650px";
+    firstImg.style.transform = "rotate(0deg)"
+    secondImg.style.transform = "rotate(-30deg)"
+}
+
+var firstImg = document.getElementById("firstImg")
+var secondImg = document.getElementById("secondImg")
